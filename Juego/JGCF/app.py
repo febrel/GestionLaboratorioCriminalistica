339030b0353f46ecb5b98f506efcdbfd7b6a718f
Luis <febreluis06@gamil.com>
2020-07-29 11:58:48 -0500

    JGCF_V0.1

--- Juego/JGCF/app.py
+++ Juego/JGCF/app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request,redirect, url_for,flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+print()
 
 # Conectarse a MySQL
 app.config['MYSQL_HOST'] = 'localhost'
